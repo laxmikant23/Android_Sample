21b3181b89f217b4db4184cea1f035ae8d80caa5
Laxmikant Nuchchi <M1031329@mindtree.com>
2017-04-13 18:43:51 +0530

    COmmit

---
+++ app/src/main/java/com/example/m1031329/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.m1031329.myapplication;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+}
---
+++ app/src/main/java/com/example/m1031329/myapplication/SampleActivity.java
@@ -0,0 +1,8 @@
+package com.example.m1031329.myapplication;
+
+/**
+ * Created by M1031329 on 2/8/2017.
+ */
+
+public class SampleActivity {
+}
